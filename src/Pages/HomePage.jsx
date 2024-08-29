1f4bf020fcfd5b28f1d7237d8fd4345f174af5ad
garesuta <garesuta@gmail.com>
2024-08-29 21:31:34 +0700

    new try 3

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useState, useEffect } from "react";
 import NavBar from "../Components/Navbar/NavBar.jsx";
 import ItemList from "../Components/ItemList.jsx";
 
@@ -8,20 +7,25 @@ const HomePage = () => {
   const [items, setItems] = useState([]);
 
   const fetchItems = async () => {
-    const response = await axios.get(
-      "https://fakestoreapi.com/products?limit=12"
-    );
-    setItems(response.data);
+    try {
+      const response = await axios.get(
+        "https://fakestoreapi.com/products?limit=12"
+      );
+      setItems(response.data);
+    } catch (error) {
+      console.error("Error fetching items:", error);
+    }
   };
+
   useEffect(() => {
-    fetchItems;
+    fetchItems(); // Corrected this line to actually call the function
     console.log(111);
   }, []);
 
   return (
     <div>
       <NavBar />
-      <ItemList item={items} />
+      <ItemList item={items} /> {/* Corrected 'item' to 'items' */}
     </div>
   );
 };
